3041e71681f1e1403691b67b33c632773c93335a
Keisuke Miura <193408231+ksukemiura@users.noreply.github.com>
2025-08-31 15:03:16 +0900

    Modify the app's title and description

--- app/layout.tsx
+++ app/layout.tsx
@@ -9,8 +9,8 @@ const defaultUrl = process.env.VERCEL_URL
 
 export const metadata: Metadata = {
   metadataBase: new URL(defaultUrl),
-  title: "Next.js and Supabase Starter Kit",
-  description: "The fastest way to build apps with Next.js and Supabase",
+  title: "Quiz Maker",
+  description: "Create, share, and play interactive quizzes with ease. Our Quiz Maker app lets you design multiple-choice quizzes, track scores, and engage learners in real time. Perfect for education, training, or fun challenges.",
 };
 
 const geistSans = Geist({
